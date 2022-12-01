36a317f64d596f07f5d738d3c00b029d36c385a5
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2022-12-01 11:26:33 +0100

    created about us page

---
+++ js/receipe.js
@@ -0,0 +1,9 @@
+//recipe page section diet
+
+const btnDietEl = document.querySelector(".btn-diet");
+const dietEl = document.querySelector(".section-diet");
+btnDietEl.addEventListener("click", function () {
+  dietEl.classList.toggle("diet-close");
+});
+
+console.log("hello world!");
